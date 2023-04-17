9e9fa1a51bfe79cc7bfbc7fbfee6f07b486bad29
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2023-04-18 04:43:48 +0900

    set homepage

--- src/atoms/event.ts
+++ src/atoms/event.ts
@@ -11,7 +11,7 @@ const hashAtom = atomWithHash("event", "不忍の心", {
 export const eventDataAtom = atom<Promise<FetchedEventData>>(async (get) => {
   const event = get(hashAtom);
   // TODO: check status code
-  const resp = await fetch(`/data/${event}.json`);
+  const resp = await fetch(`/bluevent/data/${event}.json`);
   try {
     return { ...(await resp.json()), ok: true };
   } catch (e) {
