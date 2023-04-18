97e0db8604f30c69964c87adbbd7f6002b80ca9d
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2023-04-18 22:56:29 +0900

    add EventListPlugin

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,62 @@
+const fs = require("fs");
+const path = require("path");
+
+const getEvents = (base, next) => {
+  const compare = (a, b) => {
+    if (a[1] > b[1]) return -1;
+    if (a[1] < b[1]) return 1;
+    return 0;
+  };
+
+  fs.readdir(base, (err, files) => {
+    if (err) throw err;
+
+    const events = files
+      .filter((v) => path.extname(v) === ".json")
+      .map((f) => [
+        f,
+        new Date(
+          JSON.parse(fs.readFileSync(path.join(base, f), "utf-8")).start
+        ),
+      ])
+      .sort(compare)
+      .map((v) => path.basename(v[0], ".json"));
+
+    next(events);
+  });
+};
+
+class EventListPlugin {
+  static defaultOptions = { outputFile: "events.json", baseDir: "public/data" };
+
+  constructor(options = {}) {
+    this.options = { ...EventListPlugin.defaultOptions, ...options };
+  }
+
+  apply(compiler) {
+    const pluginName = EventListPlugin.name;
+    const { webpack } = compiler;
+    const { RawSource } = webpack.sources;
+
+    compiler.hooks.emit.tapAsync(pluginName, (compilation, callback) => {
+      getEvents(this.options.baseDir, (events) => {
+        compilation.emitAsset(
+          this.options.outputFile,
+          new RawSource(JSON.stringify(events))
+        );
+        callback();
+      });
+    });
+  }
+}
+
 module.exports = {
   webpack: {
     configure: {
       resolve: {
         fallback: { path: false, fs: false, crypto: false },
       },
+      plugins: [new EventListPlugin({ outputFile: "events.json" })],
     },
   },
 };
--- src/atoms/event.ts
+++ src/atoms/event.ts
@@ -2,16 +2,44 @@ import { atom } from "jotai";
 import { FetchedEventData } from "../types";
 import { atomWithHash } from "jotai-location";
 
-const hashAtom = atomWithHash("event", "不忍の心", {
-  serialize: (v) => v,
-  deserialize: (v) => v,
+const PUBLIC_URL = process.env.PUBLIC_URL || "";
+
+const hashAtom = atomWithHash<string | null>("event", null, {
+  serialize: (v) => JSON.stringify(v),
+  deserialize: (v) => JSON.parse(v),
   setHash: "replaceState",
 });
 
+export const eventNamesAtom = atom<
+  Promise<
+    { ok: true; body: string[] } | { ok: false; title: string; body: string }
+  >
+>(async (get) => {
+  const url = `${PUBLIC_URL}/events.json`;
+  const resp = await fetch(url);
+
+  if (resp.status !== 200) {
+    return { title: `${resp.status} ${resp.statusText}`, body: url, ok: false };
+  }
+  try {
+    return { ok: true, body: await resp.json() };
+  } catch (e) {
+    return { title: `invalid JSON: ${url}`, body: `${e}`, ok: false };
+  }
+});
+
 export const eventDataAtom = atom<Promise<FetchedEventData>>(async (get) => {
-  const event = get(hashAtom);
-  const public_url = process.env.PUBLIC_URL || "";
-  const url = `${public_url}/data/${event}.json`;
+  let event = get(hashAtom);
+  if (event === null) {
+    const evresp = await get(eventNamesAtom);
+    if (evresp.ok) {
+      event = evresp.body[0];
+    } else {
+      return evresp;
+    }
+  }
+
+  const url = `${PUBLIC_URL}/data/${event}.json`;
   const resp = await fetch(url);
   if (resp.status !== 200) {
     return { title: `${resp.status} ${resp.statusText}`, body: url, ok: false };
@@ -19,7 +47,7 @@ export const eventDataAtom = atom<Promise<FetchedEventData>>(async (get) => {
   try {
     return { ...(await resp.json()), ok: true };
   } catch (e) {
-    return { title: "invalid JSON", body: `${e}`, ok: false };
+    return { title: `invalid JSON: ${url}`, body: `${e}`, ok: false };
   }
 });
 
